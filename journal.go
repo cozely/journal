27cf4b6292ffd91cd0aaaff3ed18d7b68a2f135c
drakmaniso <moussault.laurent@gmail.com>
2018-12-30 13:19:50 +0100

    Improve

--- journal.go
+++ journal.go
@@ -8,39 +8,55 @@ import (
 
 ////////////////////////////////////////////////////////////////////////////////
 
-var (
-	outbuf = bytes.NewBuffer(make([]byte, 0, 256))
-	errbuf = bytes.NewBuffer(make([]byte, 0, 256))
-)
+type Journal struct {
+	prefix string
+	buf *bytes.Buffer
+	info, warn, debug io.Writer
+}
 
-////////////////////////////////////////////////////////////////////////////////
-
-type Journal string
+func New(prefix string, info, warn, debug io.Writer) *Journal {
+	return &Journal{
+		prefix: prefix,
+		buf: bytes.NewBuffer(make([]byte, 0, 256)),
+		info: info,
+		warn: warn,
+		debug: debug,
+	}
+}
 
 func (j Journal) Info(msg ...string) {
-	if j != "" {
-		write(outbuf, string(j))
-		writeln(outbuf, msg...)
-		os.Stdout.Write(outbuf.Bytes())
-		outbuf.Reset()
+	if j.info != nil {
+		write(j.buf, j.prefix)
+		writeln(j.buf, msg...)
+		os.Stdout.Write(j.buf.Bytes())
+		j.buf.Reset()
 	}
 }
 
 func (j Journal) Warn(msg ...string) {
-	if j != "" {
-		write(errbuf, string(j))
-		writeln(errbuf, msg...)
-		os.Stderr.Write(errbuf.Bytes())
-		errbuf.Reset()
+	if j.warn != nil {
+		write(j.buf, j.prefix)
+		writeln(j.buf, msg...)
+		os.Stderr.Write(j.buf.Bytes())
+		j.buf.Reset()
 	}
 }
 
 func (j Journal) Debug(msg ...string) {
-	if j != "" {
-		write(errbuf, string(j))
-		writeln(errbuf, msg...)
-		os.Stderr.Write(errbuf.Bytes())
-		errbuf.Reset()
+	if j.debug != nil {
+		write(j.buf, j.prefix)
+		writeln(j.buf, msg...)
+		os.Stderr.Write(j.buf.Bytes())
+		j.buf.Reset()
+	}
+}
+
+func (j Journal) Err(err error) {
+	if j.debug != nil {
+		write(j.buf, j.prefix)
+		writeln(j.buf, err.Error())
+		os.Stderr.Write(j.buf.Bytes())
+		j.buf.Reset()
 	}
 }
 
